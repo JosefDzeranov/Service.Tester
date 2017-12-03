9de65bfa7b6255cd0a6a4dc5b46c92b22b2fcf1a
Josef Dzeranov <soslan29@gmail.com>
2017-12-03 18:46:15 +0300

    Add MVC

---
+++ Service.Tester/Service.Tester/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Tester.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Get()
+        {
+            return "Yes!!!";
+        }
+    }
+}
--- Service.Tester/Service.Tester/Startup.cs
+++ Service.Tester/Service.Tester/Startup.cs
@@ -2,17 +2,29 @@
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Service.Tester
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             var serverAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
 
             app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
