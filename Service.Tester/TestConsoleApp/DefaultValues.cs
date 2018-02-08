963da86ace3b351ac719e5d07b369d0d62661788
Josef Dzeranov <soslan29@gmail.com>
2018-02-08 22:38:37 +0300

    refact

---
+++ Service.Tester/TestConsoleApp/DefaultValues.cs
@@ -0,0 +1,18 @@
+﻿namespace TestConsoleApp
+{
+    public static class DefaultValues
+    {
+        static string sourceCode = @"
+using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello world"");
+        }
+    }
+}";
+    }
+}
---
+++ Service.Tester/TestConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
