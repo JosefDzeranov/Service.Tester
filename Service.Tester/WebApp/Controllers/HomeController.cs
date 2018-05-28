4f65319ac91ce2223c28852a1753d2c3761fa870
Josef Dzeranov <soslan29@gmail.com>
2018-05-28 08:15:54 +0300

    Избавился от кнопки которая генерировала мне начальные данные

--- Service.Tester/WebApp/Controllers/HomeController.cs
+++ Service.Tester/WebApp/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Service.Storage.Context;
-using Service.Storage.Entities;
-using Service.Storage.ExtraModels;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -39,26 +36,6 @@ namespace WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult FirstStart()
-        {
-            _dbContext.ProblemTypes.AddRange(GetAllProblemTypes());
-            _dbContext.SaveChanges();
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        private List<ProblemType> GetAllProblemTypes()
-        {
-            return new List<ProblemType>
-            {
-                new ProblemType {Type = ProblemTypes.None, Name = "Неизвестно"},
-                new ProblemType {Type = ProblemTypes.TraceTable, Name = "Трассировочная таблица"},
-                new ProblemType {Type = ProblemTypes.BlackBox, Name = "Черный ящик"},
-                new ProblemType {Type = ProblemTypes.RestoreData, Name = "Восстановление данных"},
-                new ProblemType {Type = ProblemTypes.CodeCorrector, Name = "Исправление кода"},
-            };
         }
     }
 }
